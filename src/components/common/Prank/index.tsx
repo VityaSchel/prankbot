cca5d5b72dd35a960ccb2f9547ba8a3c4cf83d48
hloth <gitlab@hloth.dev>
2023-03-21 22:36:49 +0300

    Pranks

---
+++ src/components/common/Prank/index.tsx
@@ -0,0 +1,36 @@
+import styles from './styles.module.scss'
+import background1 from './assets/background1.png'
+import background2 from './assets/background2.png'
+import background3 from './assets/background3.png'
+import background4 from './assets/background4.png'
+import background5 from './assets/background5.png'
+import seedrandom from 'seedrandom'
+import Image from 'next/image'
+
+export type Prank = {
+  id: string
+  title: string
+  statistics: number
+}
+
+export default function PrankPreview(props: { prank: Prank }) {
+  const bg = [
+    background1,
+    background2,
+    background3,
+    background4,
+    background5
+  ][Math.floor(seedrandom(props.prank.id)() * 5)]
+
+  return (
+    <div className={styles.prank}>
+      <div className={styles.image}>
+        <Image src={bg} fill alt='' />
+      </div>
+      <div className={styles.info}>
+        <h3>{props.prank.title}</h3>
+        <span>{props.prank.statistics} розыгрышей</span>
+      </div>
+    </div>
+  )
+}
--- src/components/homepage/Pranks/index.tsx
+++ src/components/homepage/Pranks/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import styles from './styles.module.scss'
 import { Tabs, Skeleton } from 'antd'
+import PrankPreview, { Prank } from '@/components/common/Prank'
 
 type Category = {
   id: number
@@ -11,6 +12,7 @@ type Category = {
 export default function Pranks() {
   const [categories, setCategories] = React.useState<null | Category[]>(null)
   const [activeCategory, setActiveCategory] = React.useState(0)
+  const [pranks, setPranks] = React.useState<null | Prank[]>(null)
 
   const mapItems = (categories: Category[]) => {
     return categories.map(category => ({
@@ -39,6 +41,14 @@ export default function Pranks() {
         },
       ])
     }, 1000)
+    setPranks(new Array(Math.floor(Math.random()*50)+10)
+      .fill(null)
+      .map(() => ({
+        id: String(Math.floor(Math.random()*Number.MAX_SAFE_INTEGER)),
+        title: 'Название розыгрыша',
+        statistics: Math.floor(Math.random()*300)
+      }))
+    )
   }, [])
 
   return (
@@ -54,6 +64,18 @@ export default function Pranks() {
           <Skeleton />
         )
       }
+      <div className={styles.pranksPreviewsList}>
+        {categories && (pranks != null) && pranks.map(prank => (
+          <PrankPreview
+            key={prank.id}
+            prank={{
+              id: prank.id,
+              title: prank.title,
+              statistics: prank.statistics,
+            }}
+          />
+        ))}
+      </div>
     </div>
   )
 }
