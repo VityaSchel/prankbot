51dddff84c2bbaeaf0d811cf9714f13f24090efb
hloth <gitlab@hloth.dev>
2023-03-25 14:57:21 +0300

    Modal fix

--- src/components/common/Footer/SubscriptionManagement/index.tsx
+++ src/components/common/Footer/SubscriptionManagement/index.tsx
@@ -1,23 +1,50 @@
 import React from 'react'
 import styles from './styles.module.scss'
-import { Button } from 'antd'
 import Modal from '@/components/common/Modal'
+import { Button as AntdButton } from 'antd'
+import Button from '@/components/common/Button'
 
 export default function SubscriptionManagemnt() {
+  const [screen, setScreen] = React.useState<'info' | 'prompt'>('info')
   const [dialogVisible, setDialogVisible] = React.useState(false)
+
+  React.useEffect(() => {
+    if(!dialogVisible) setScreen('info')
+  }, [dialogVisible])
+
+  const handleUnsubscribe = () => {
+    alert('¯\\_(ツ)_/¯')
+  }
 
   return (
     <>
-      <Button type='text' className={styles.link} onClick={() => setDialogVisible(true)}>
+      <AntdButton type='text' className={styles.link} onClick={() => setDialogVisible(true)}>
         Управление подпиской
-      </Button>
+      </AntdButton>
       <Modal open={dialogVisible} onClose={() => setDialogVisible(false)}>
-        <h2 className={styles.title}>Управляйте своей подпиской</h2>
-        <div className={styles.subscriptionInfo}>
-          <span className={styles.price}>29 рублей</span>
-          <span className={styles.label}>Стоимость подписки</span>
-        </div>
-        <Button type='text'>Отменить подписку</Button>
+        {screen === 'info'
+          ? (
+            <>
+              <h2 className={styles.title}>Управляйте своей подпиской</h2>
+              <div className={styles.subscriptionInfo}>
+                <span className={styles.price}>29 рублей</span>
+                <span className={styles.label}>Стоимость подписки</span>
+              </div>
+              <AntdButton type='text' onClick={() => setScreen('prompt')}>Отменить подписку</AntdButton>
+            </>
+          ) : (
+            <>
+              <div className={styles.topBar}>
+                <h2 className={styles.title}>Отмена подписки</h2>
+                <p>Уверены, что хотите отменить подписку?</p>
+              </div>
+              <div className={styles.buttons}>
+                <Button variant='secondary' onClick={handleUnsubscribe}>Отменить</Button>
+                <Button variant='primary' onClick={() => setDialogVisible(false)}>Не отменять</Button>
+              </div>
+            </>
+          )
+        }
       </Modal>
     </>
   )
