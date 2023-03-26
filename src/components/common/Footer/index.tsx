fe8e66dcbce4b79ac402fb39eecb1e30587dbac4
hloth <gitlab@hloth.dev>
2023-03-26 22:50:45 +0300

    Law info

--- src/components/common/Footer/index.tsx
+++ src/components/common/Footer/index.tsx
@@ -6,6 +6,9 @@ export default function Footer() {
   return (
     <footer className={styles.footer}>
       <SubscriptionManagement />
+      <p className={styles.law}>
+        {`ООО "ГТТ"\nИНН: 7839112438 ОГРН: 1197847020580\n+7 (495) 846-77-03`}
+      </p>
     </footer>
   )
 }
