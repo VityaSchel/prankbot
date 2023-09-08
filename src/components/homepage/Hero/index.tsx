9bc7a70d2e5ff267b9c5c8de4de0db42b4641577
Aleksandr <aleksandr@MacBook-Pro-Aleksandr.local>
2023-09-08 15:41:37 +0300

    fixed

--- src/components/homepage/Hero/index.tsx
+++ src/components/homepage/Hero/index.tsx
@@ -14,8 +14,9 @@ export default function Hero() {
     <div className={styles.hero}>
       <div className={styles.info}>
         <div className={styles.text}>
-          <h1>Разыграй своих друзей <wbr />или коллег с помощью <span className={styles.highlight}>ПранкБота</span></h1>
-          <h2>Закажи розыгрыш по телефону и получи запись разговора</h2>
+            <h1>Разыграй друга <wbr />по телефону и получи <span className={styles.highlight}>запись разговора</span></h1>
+          {/*<h1>Разыграй своих друзей <wbr />или коллег с помощью <span className={styles.highlight}>ПранкБота</span></h1>*/}
+          <h2>Сервис розыгрышей по телефону позволяет выбрать смешную аудиозапись и совершить звонок, после чего робот пришлет полную запись разговора. В коллекции сервиса есть десятки смешных аудио для розыгрышей и система, которая позволяет отслеживать звонок в режиме реального времени. Подари своим друзьям, родственникам и знакомых незабываемый опыт.</h2>
         </div>
         {/* <Button type="primary" size='large'>Выбрать розыгрыш</Button> */}
         <Button variant='secondary' onClick={handleScroll}>Выбрать розыгрыш <BsArrowDown /></Button>
